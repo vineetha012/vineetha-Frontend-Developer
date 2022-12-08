25cf387e1680e1d664bcc0d35eb5be8d8640a144
vineetha <yenugulavineetha2001@gmail.com>
2022-12-08 17:10:44 +0530

    Added capsule cards

---
+++ src/components/cardContainer.js
@@ -0,0 +1,46 @@
+import axios from "axios";
+import React, { useContext, useEffect } from "react";
+import { Card } from "./card";
+import { SpaceXContext } from "./context";
+import { Pagination } from "./Pagination";
+export const CardContainer=()=>{
+    const {capsuleData,setCapsuleData,currentpage, setcurrentpage,Itemperpage,  searchval,setSearchval}=useContext(SpaceXContext)
+    const { getdata}=useContext(SpaceXContext)
+   
+    const keys=["status","type"]
+    const HandleSearch=(data)=>{
+        return data.filter((item)=>keys.some((key)=>item[key].toLowerCase().includes(searchval)))
+        //return data.filter((field)=> parak.some((val)=>field[val].toLowerCase().includes(searchval)))
+    }
+    // useEffect(()=>{
+    //     getdata()
+    // },[])
+    const indexOfLastItem = currentpage * Itemperpage;
+    const indexOfFirstItem = indexOfLastItem - Itemperpage;
+    const currentItems=capsuleData.slice(indexOfFirstItem,indexOfLastItem)
+    console.log(searchval)
+    console.log(HandleSearch(capsuleData))
+    return(
+        <>
+            <div className="cars-container">
+                {searchval ? HandleSearch(capsuleData).map((data,key)=>{
+                    console.log(data)
+                    return(
+                        <>
+                        <Card key={data.capsule_serial} data={data} />
+                        </>
+                    )
+                })
+                :currentItems.map((data,key)=>{
+                    return(
+                        <>
+                        <Card key={data.capsule_serial} currentItems={currentItems} data={data}/>
+                        </>
+                    )
+                })
+                }
+            </div>
+            
+        </>
+    )
+}
